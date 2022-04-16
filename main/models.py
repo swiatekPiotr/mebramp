db04a05c07a1b10fd40f76250491f912301c5701
swiatek1995@gmail.com <swiatek1995@gmail.com>
2022-04-16 18:21:46 +0200

    models to db

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Categories(models.Model):
+    title = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.title
+
+
+class Products(models.Model):
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    textile = models.CharField(max_length=60)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return self.name
