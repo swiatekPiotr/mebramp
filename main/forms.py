9f7c44ca44cb4498d63ea05594a35dc4c31a4db2
swiatek1995@gmail.com <swiatek1995@gmail.com>
2022-04-26 19:31:52 +0200

    prettier form prev ability

--- main/forms.py
+++ main/forms.py
@@ -6,5 +6,14 @@ from .models import Products
 class ProductsForm(ModelForm):
     class Meta:
         model = Products
-        fields = ('category', 'name', 'product_image1', 'product_image2', 'product_image3',
-                  'description', 'textile', 'price')
+        fields = ['category', 'name', 'product_image1', 'product_image2', 'product_image3',
+                  'description', 'textile', 'price']
+        labels = {
+            'product_image1': 'img 1', 'product_image2': 'img 2', 'product_image3': 'img 3',
+            'description': '', 'textile': '', 'price': '',
+        }
+        widgets = {
+            'description': forms.Textarea(attrs={'placeholder': 'short description about product'}),
+            'textile': forms.TextInput(attrs={'placeholder': 'textile'}),
+            'price': forms.NumberInput(attrs={'placeholder': 'price'}),
+        }
